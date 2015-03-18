dfaa9ed8082a007e7b36714b3a21898218bbed76
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-03-18 23:42:09 +0200

    Bit of cleanup.

--- src/hunter/actions.py
+++ src/hunter/actions.py
@@ -4,6 +4,7 @@ import linecache
 
 DEFAULT_MIN_FILENAME_ALIGNMENT = 15
 
+
 def _safe_getline(filename, lineno, getline=linecache.getline):
     try:
         return getline(filename, lineno)
@@ -11,29 +12,61 @@ def _safe_getline(filename, lineno, getline=linecache.getline):
         return "??? no source: {} ???".format(exc)
 
 
-def print_code(context, frame, kind, arg, stream=sys.stderr, basename=os.path.basename, min_filename_alignment=DEFAULT_MIN_FILENAME_ALIGNMENT):
-    filename = frame.f_code.co_filename or "<???>"
-    alignment = context.filename_alignment = max(getattr(context, 'filename_alignment', min_filename_alignment), len(filename))
-    stream.write("{:>{align}}:{:<5} {:11} {}{}{}\n".format(
-        basename(filename),
-        frame.f_lineno,
-        kind,
-        _safe_getline(filename, frame.f_lineno).rstrip(),
-        "  # => " if arg else "", arg or "",
-        align=alignment
-    ))
-    return context
+class CodePrinter(object):
+    def __init__(self, stream=sys.stderr, filename_alignment=DEFAULT_MIN_FILENAME_ALIGNMENT):
+        self.stream = stream
+        self.filename_alignment = filename_alignment
+
+    def __call__(self, event, basename=os.path.basename):
+        filename = event.filename or "<???>"
+        # TODO: support auto-alignment, need a context object for this, eg:
+        # alignment = context.filename_alignment = max(getattr(context, 'filename_alignment', self.filename_alignment), len(filename))
+        self.stream.write("{:>{align}}:{:<5} {:9} {}\n".format(
+            basename(filename),
+            event.lineno,
+            event.kind,
+            _safe_getline(filename, event.lineno).rstrip(),
+            align=self.filename_alignment
+        ))
+        if event.kind in ('return', 'exception'):
+            self.stream.write("{:>{align}}       {:9} {} value: {}\n".format(
+                "",
+                ".",
+                event.kind,
+                event.arg,
+                align=self.filename_alignment
+            ))
 
 
-def print_vars(*predicates, min_filename_alignment=DEFAULT_MIN_FILENAME_ALIGNMENT):
-    def vars_printer(context, frame, kind, arg)
-        , stream=sys.stderr):
-    alignment = getattr(context, 'filename_alignment', min_filename_alignment)
-    stream.write("{:>{align}}       {:11} {} = {}\n".format(
-        "",
-        "vars",
-        "  # => " if arg else "", arg or "",
-        align=alignment
-    ))
+class VarsDumper(object):
+    def __init__(self, name=None, names=(), globals=False, stream=sys.stderr, filename_alignment=DEFAULT_MIN_FILENAME_ALIGNMENT):
+        self.stream = stream
+        self.filename_alignment = filename_alignment
+        self.names = list(names)
+        if name:
+            self.names.append(name)
+        self.globals = globals
 
-
+    def __call__(self, event):
+        first = True
+        for key, value in event.locals.items():
+            if key in self.names:
+                self.stream.write("{:>{align}}       {:9} {} -> {}\n".format(
+                    "",
+                    "vars" if first else ".",
+                    key,
+                    value,
+                    align=self.filename_alignment
+                ))
+                first = False
+        if self.globals:
+            for key, value in event.globals.items():
+                if key in self.names:
+                    self.stream.write("{:>{align}}       {:9} {} => {}\n".format(
+                        "",
+                        "vars" if first else ".",
+                        key,
+                        value,
+                        align=self.filename_alignment
+                    ))
+                    first = False
