b840364048b4c81039cd267a9111ba9e45096385
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-12-19 06:38:41 +0200

    Styling.

--- src/hunter/const.py
+++ src/hunter/const.py
@@ -1,7 +1,6 @@
 import sys
 from distutils.sysconfig import get_python_lib
 
-
 SITE_PACKAGES_PATH = get_python_lib()
 SYS_PREFIX_PATHS = (
     sys.prefix,
@@ -11,5 +10,3 @@ SYS_PREFIX_PATHS = (
 for prop in 'real_prefix', 'real_exec_prefix', 'base_prefix', 'base_exec_prefix':
     if hasattr(sys, prop):
         SYS_PREFIX_PATHS += getattr(sys, prop),
-
-
