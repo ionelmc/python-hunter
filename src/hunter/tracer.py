d86b1163c87e67a69f76303ea84f35fa6fd1107f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-12-20 14:24:06 +0200

    Make these fields private.

--- src/hunter/tracer.py
+++ src/hunter/tracer.py
@@ -12,13 +12,13 @@ class Tracer(object):
     """
 
     def __init__(self):
-        self._handler = None
-        self._previous = None
+        self.__handler = None
+        self.__previous = None
 
     def __str__(self):
-        return "Tracer(_handler={}, _previous={})".format(
-            "<stopped>" if self._handler is None else self._handler,
-            self._previous,
+        return "Tracer(handler={}, previous={})".format(
+            "<stopped>" if self._handler is None else self.__handler,
+            self.__previous,
         )
 
     def __call__(self, frame, kind, arg):
@@ -31,8 +31,8 @@ class Tracer(object):
             because it might
             match further inside.
         """
-        if self._handler is not None:
-            self._handler(Event(frame, kind, arg, self))
+        if self.__handler is not None:
+            self.__handler(Event(frame, kind, arg, self))
             return self
 
     def trace(self, predicate):
@@ -45,8 +45,8 @@ class Tracer(object):
             predicates (:class:`hunter.Q` instances): Runs actions if any of the given predicates match.
             options: Keyword arguments that are passed to :class:`hunter.Q`, for convenience.
         """
-        self._handler = predicate
-        self._previous = sys.gettrace()
+        self.__handler = predicate
+        self.__previous = sys.gettrace()
         sys.settrace(self)
         return self
 
@@ -54,9 +54,9 @@ class Tracer(object):
         """
         Stop tracing.
         """
-        if self._handler is not None:
-            sys.settrace(self._previous)
-            self._handler = self._previous = None
+        if self.__handler is not None:
+            sys.settrace(self.__previous)
+            self.__handler = self.__previous = None
 
     def __enter__(self):
         return self
