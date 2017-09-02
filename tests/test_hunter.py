7f5f776a7669435a37fd2ca791940e55f26b55d5
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-09-02 11:38:29 +0300

    Now works on py3.

--- tests/test_hunter.py
+++ tests/test_hunter.py
@@ -929,7 +929,9 @@ def tracer_impl(request):
 
 def _bulky_func_that_use_stdlib():
     import difflib
-    list(difflib.unified_diff(map(str, range(2000)), map(str, range(0, 2000, 2)), 'a', 'b'))
+    a = list(map(str, range(2000)))
+    b = list(map(str, range(0, 2000, 2)))
+    list(difflib.unified_diff(a, b, 'a', 'b'))
 
 
 def test_perf_filter(tracer_impl, benchmark):
