bf97f8e1890aedbed337516f9b07de6813bdf3d2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-13 00:58:36 +0200

    Loosen up assertion: allow ordering to be messed up.

--- tests/test_hunter.py
+++ tests/test_hunter.py
@@ -1183,7 +1183,7 @@ def test_pid_prefix(LineMatcher, Action, force_pid, capfd):
     print('ERR', err)
     lm = LineMatcher(out.splitlines())
     prefix = '[[]*[]] *' if force_pid else ''
-    lm.fnmatch_lines([
+    lm.fnmatch_lines_random([
         prefix + "MainThread  *test_hunter.py:*  line * a = 1",
         prefix + "MainThread  *test_hunter.py:*  line * if pid:",
         prefix + "MainThread  *               *  vars * a => 1",
