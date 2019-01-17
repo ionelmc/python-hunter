7345da9e7bc8a7d4ee1439acc4698f9db7a8830f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-17 12:50:07 +0200

    Simplify this.

--- ci/appveyor-bootstrap.py
+++ ci/appveyor-bootstrap.py
@@ -108,4 +108,4 @@ def install_packages(home, *packages):
 if __name__ == "__main__":
     install_python(environ['PYTHON_VERSION'], environ['PYTHON_ARCH'], environ['PYTHON_HOME'])
     install_pip(environ['PYTHON_HOME'])
-    install_packages(environ['PYTHON_HOME'], "setuptools>=18.0.1", "tox-wheel", "virtualenv>=13.1.0")
+    install_packages(environ['PYTHON_HOME'], "tox-wheel")
