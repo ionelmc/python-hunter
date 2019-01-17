9fecda463b1588b7908ffadb882bbcfe5fb4ece4
Ionel Cristian Mărieș <contact@ionelmc.ro>
2019-01-17 17:43:09 +0200

    Publish artifacts through test pypi on appveyor as well.

--- ci/appveyor-bootstrap.py
+++ ci/appveyor-bootstrap.py
@@ -108,4 +108,4 @@ def install_packages(home, *packages):
 if __name__ == "__main__":
     install_python(environ['PYTHON_VERSION'], environ['PYTHON_ARCH'], environ['PYTHON_HOME'])
     install_pip(environ['PYTHON_HOME'])
-    install_packages(environ['PYTHON_HOME'], "tox-wheel")
+    install_packages(environ['PYTHON_HOME'], "tox-wheel", "twine")
