828ee62a82948acb3de33eff65a9f1aff7245796
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-01-26 23:15:19 +0200

    Remove bogus dep (forgot to remove it after vendoring).

--- setup.py
+++ setup.py
@@ -158,7 +158,6 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
-        'colorama',
     ],
     extras_require={
         ':platform_system != "Windows"': ['manhole >= 1.5'],
