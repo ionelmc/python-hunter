f42806f0c11ea23447c538e02b6deb0d2d46040b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-06-23 15:54:00 +0300

    Bump version: 3.3.6 → 3.3.7

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ try:
     version = release = get_distribution('hunter').version
 except Exception:
     traceback.print_exc()
-    version = release = '3.3.6'
+    version = release = '3.3.7'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/hunter/__init__.py
+++ src/hunter/__init__.py
@@ -45,7 +45,7 @@ except ImportError:
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = '3.3.6'
+    __version__ = '3.3.7'
 
 __all__ = (
     'And',
