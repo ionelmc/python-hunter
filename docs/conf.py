4080c4fd0a6a8ccd70d9912fb1b083222138b816
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-12-14 20:47:28 +0200

    Bump version: 3.4.0 → 3.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ try:
     version = release = get_distribution('hunter').version
 except Exception:
     traceback.print_exc()
-    version = release = '3.4.0'
+    version = release = '3.4.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
