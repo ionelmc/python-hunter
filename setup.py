9e300c152dccef694296b47628f32e84d1ff7f64
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-09-27 12:20:55 +0300

    Bump version: 3.5.0 → 3.5.1

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     use_scm_version={
         'local_scheme': 'dirty-tag',
         'write_to': 'src/hunter/_version.py',
-        'fallback_version': '3.5.0',
+        'fallback_version': '3.5.1',
     },
     license='BSD-2-Clause',
     description='Hunter is a flexible code tracing toolkit.',
--- src/hunter/__init__.py
+++ src/hunter/__init__.py
@@ -45,7 +45,7 @@ except ImportError:
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = '3.5.0'
+    __version__ = '3.5.1'
 
 __all__ = (
     'And',
